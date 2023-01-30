b29c3715f39683adaa907b5699af8eea0b1f4bd1
r7truong <raytruemail@gmail.com>
2023-01-30 20:42:14 +0000

    Update MainApp.java
    Updated

--- java/src/main/java/ca/uwaterloo/cs489/exercise2/MainApp.java
+++ java/src/main/java/ca/uwaterloo/cs489/exercise2/MainApp.java
@@ -1,18 +1,15 @@
 package ca.uwaterloo.cs489.exercise2;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.FileNotFoundException;
-
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 public class MainApp {
 
@@ -34,8 +31,16 @@ public class MainApp {
       // Iterate over all of the files in the directory, creating a job for each
       for (Path entry : ds) {
         Job job = new Job(entry.toFile());
-        logger.info(String.format("Job %d yields %d\n", job.getInput(), job.processJob()));
+        logger.info(String.format("Job %d yields %d\n", job.getInput(),
+                                  job.processJob()));
+        job.deleteFile();
+        logger.info(String.format("Deleted a processed job file.\n"));
       }
+
+      logger.info(String.format(
+          "All jobs have been processed. Deleting temporary directory.\n"));
+      Files.delete(dir);
+
     } catch (IOException e) {
       e.printStackTrace();
     }
